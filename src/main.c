73737392145c77bcc68db101d620149f2394be00
CRaph0705 <raphael.cochrane94@gmail.com>
2025-08-12 10:52:20 +0200

    tests with threads

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 10:55:24 by rcochran          #+#    #+#             */
-/*   Updated: 2025/07/17 15:21:06 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/08/11 14:50:19 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,29 +28,22 @@ void	main_func(int nb_philo,
 void	main_func(int nb_philo,
 			int time_to_die, int time_to_eat, int time_to_sleep)
 {
-	(void)nb_philo;
-	printf("time_to_die = %d, time_to_eat = %d,\
-		 time_to_sleep = %d\n", time_to_die, time_to_eat, time_to_sleep);
+	printf("nb_philo = %d, time_to_die = %d, time_to_eat = %d,\
+		 time_to_sleep = %d\n",nb_philo, time_to_die, time_to_eat, time_to_sleep);
 }
 
-int	new_philosoph(void)
-{
-	t_philo	*philo;
 
-	philo = malloc(sizeof(t_philo));
-	if (!philo)
-	{
-		return (1);
-	}
-	return (0);
-}
+pthread_mutex_t	mutex;
 
-int	main(int ac, char **av)
+/* int	main(int ac, char **av)
 {
-	int	philo_nb;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
+	pthread_t	p1;
+	int			philo_nb;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+
+	pthread_mutex_init(&mutex, NULL);
 
 	if (ac < 5)
 		return (printf("Usage : ./philo <numer_of_philosophers> <time_to_die>\
@@ -62,4 +55,24 @@ int	main(int ac, char **av)
 	time_to_sleep = ft_atoi(av[4]);
 	main_func(philo_nb, time_to_die, time_to_eat, time_to_sleep);
 	return (0);
+} */
+
+void	routine(void)
+{
+	printf("Coucou je suis un thread\n");
+	sleep(3);
+	printf("fin du thread\n");
 }
+int main(void)
+{
+	pthread_t t1, t2;
+	if (!pthread_create(&t1, NULL, &routine, NULL))
+		return (1);
+	if (!pthread_create(&t2, NULL, &routine, NULL))
+		return (1);
+	if (!pthread_join(t1, NULL))
+		return (1);
+	if (!pthread_join(t2, NULL))
+		return (1);
+	return (0);
+}		
