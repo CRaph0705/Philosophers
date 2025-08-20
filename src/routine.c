70d2e140e5852e9acb24917eed9006badb654329
CRaph0705 <raphael.cochrane94@gmail.com>
2025-08-20 17:50:12 +0200

    converted ms in micro seconds in usleep, sliced the sleep routine (wip) to cut if there is a dead philo

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 16:56:06 by rcochran          #+#    #+#             */
-/*   Updated: 2025/08/20 16:36:21 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/08/20 17:45:45 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ int	do_eat(t_philo *philo)
 	pthread_mutex_lock(&philo->data->mtx);
 	printf("%ld %d is eating\n", actual, philo->id);
 	pthread_mutex_unlock(&philo->data->mtx);
-	usleep(philo->time_to_eat);
+	usleep(philo->time_to_eat * 1000);
+	philo->last_meal = get_time_in_ms() - philo->start_time;
 	philo->nb_meal += 1;
-	philo->last_meal = actual;
 	put_forks(philo);
 	pthread_mutex_lock(&philo->data->mtx);
 	if (philo->nb_meal == philo->data->max_meal)
@@ -45,43 +45,74 @@ int	do_eat(t_philo *philo)
 	return (do_sleep(philo));
 }
 
+
+void	philo_sleep(time_t sleep_time)
+{
+	time_t	wake_up;
+
+	wake_up = get_time_in_ms() + sleep_time;
+	while (get_time_in_ms() < wake_up)
+	{
+		usleep(100);
+	}
+}
+
 int	do_sleep(t_philo *philo)
 {
-	time_t	actual;
+	long long	actual;
+	long long	wake_up;
 
 	pthread_mutex_lock(&philo->data->mtx);
 	if (philo->is_dead == true || philo->data->has_stopped)
-		return (pthread_mutex_unlock(&philo->data->mtx));
+		return (pthread_mutex_unlock(&philo->data->mtx), 1);
 	pthread_mutex_unlock(&philo->data->mtx);
 	actual = get_time_in_ms() - philo->start_time;
 	pthread_mutex_lock(&philo->data->mtx);
-	printf("%ld %d is sleeping\n", actual, philo->id);
+	printf("%lld %d is sleeping\n", actual, philo->id);
 	pthread_mutex_unlock(&philo->data->mtx);
-	if (actual - philo->last_meal + philo->time_to_sleep > philo->time_to_die)
+	wake_up = get_time_in_ms() + philo->time_to_sleep;
+	while (get_time_in_ms() < wake_up)
 	{
-		usleep(philo->time_to_die - (actual - philo->last_meal));
-		return (do_die(philo));
+		pthread_mutex_lock(&philo->data->mtx);
+		if (philo->is_dead || philo->data->has_stopped)
+		{
+			pthread_mutex_unlock(&philo->data->mtx);
+			return (1);
+		}
+		pthread_mutex_unlock(&philo->data->mtx);
+		usleep(1000);
 	}
-	usleep(philo->time_to_sleep);
+/* 	if (actual - philo->last_meal + philo->time_to_sleep > philo->time_to_die)
+	{
+		usleep((philo->time_to_die - (actual - philo->last_meal))* 1000);
+		return (do_die(philo));
+	} */
+/* 	usleep(philo->time_to_sleep * 1000);*/
 	return (do_think(philo));
 }
 
 int	do_think(t_philo *philo)
 {
-	time_t	actual;
-	time_t	time_to_think;
+	long	actual;
+	long	time_to_think;
 
-	time_to_think = (philo->time_to_die
-			- (get_time_in_ms() - philo->last_meal) - philo->time_to_eat) / 2;
 	pthread_mutex_lock(&philo->data->mtx);
 	if (philo->is_dead == true || philo->data->has_stopped)
-		return (pthread_mutex_unlock(&philo->data->mtx));
+	{
+		pthread_mutex_unlock(&philo->data->mtx);
+		return (1);
+	}
 	pthread_mutex_unlock(&philo->data->mtx);
 	actual = get_time_in_ms() - philo->start_time;
+	time_to_think = (philo->time_to_die
+			- (actual - philo->last_meal) - philo->time_to_eat) / 2;
+	if (time_to_think < 0)
+		time_to_think = 0;
+	printf("time to think = [%ld]\n", time_to_think);
 	pthread_mutex_lock(&philo->data->mtx);
 	printf("%ld %d is thinking\n", actual, philo->id);
 	pthread_mutex_unlock(&philo->data->mtx);
-	usleep(time_to_think);
+	usleep(time_to_think * 1000);
 	return (0);
 }
 
