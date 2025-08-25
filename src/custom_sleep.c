d1e8ee35da90f3dc5f3054235dd6a68af5af13e0
CRaph0705 <raphaelcochrane@hotmail.fr>
2025-08-26 01:36:32 +0200

    custom sleep changed, check death logic added

--- src/custom_sleep.c
+++ src/custom_sleep.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 00:52:59 by rcochran          #+#    #+#             */
-/*   Updated: 2025/08/26 00:54:13 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/08/26 01:35:54 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,9 @@ int	custom_usleep(t_philo *philo, long duration_ms)
     long start = get_time_in_ms();
     while (get_time_in_ms() - start < duration_ms)
     {
-        pthread_mutex_lock(&philo->data->mtx);
-        if (philo->is_dead || philo->data->has_stopped)
-        {
-            pthread_mutex_unlock(&philo->data->mtx);
+        if (check_death(philo))
             return (1);
-        }
-        pthread_mutex_unlock(&philo->data->mtx);
-
-        usleep(200);
+        usleep(100);
     }
     return (0);
 }
